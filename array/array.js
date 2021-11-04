517bdcb3f4dea67680c4b8e66435d5f032e1f640
ju-kkim <kimjuyeon@gimjuyeon-ui-MacBookAir.local>
2021-11-04 15:51:16 +0900

    factorial 함수 while문 사용

--- array/array.js
+++ array/array.js
@@ -1,10 +1,24 @@
 // factorial 함수 만들기
+
+// for 사용
+// function calculate(m) {
+//     let result = [];
+//     let calcNum = 1;
+//     for(let num = 1; num <= m; num++) {
+//         calcNum = calcNum * num;
+//         result.push(calcNum)
+//     }
+//     return result;
+// }
+
+// while 사용
 function calculate(m) {
     let result = [];
+    let num = 1;
     let calcNum = 1;
-    for(let num = 1; num <= m; num++) {
-        calcNum = calcNum * num;
-        result.push(calcNum)
+    while(num <= m) {
+        calcNum *= num++;
+        result.push(calcNum);
     }
     return result;
 }
