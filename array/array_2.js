1ca341d60f633e21b3c50d10c542b9dc4ad90860
ju-kkim <wndus8198@naver.com>
2021-11-04 21:23:07 +0900

    배열 거르기(map 사용)

--- array/array_2.js
+++ array/array_2.js
@@ -1,31 +1,39 @@
 const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 
+// function filterId(inputArray) {
+//     let resultArray = [];
+//     for(const people of inputArray){
+//         let changeId = removeSpecialChar(people);
+//         changeId = removeNum(changeId)
+//         resultArray.push(changeId);
+//     }
+//     return resultArray;
+// }
+
+// // 특수문자 제거
+// function removeSpecialChar(text) {
+//     let reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim;
+//     let changeText = text.replace(reg, "");
+//     return changeText;
+// }
+
+// // 숫자제거
+// function removeNum(text) {
+//     let resultText = '';
+//     for(const piece of text){
+//         if(isNaN(piece)){
+//             resultText += piece
+//         }
+//     }
+//     return resultText
+// }
+
 function filterId(inputArray) {
-    let resultArray = [];
-    for(const people of inputArray){
-        let changeId = removeSpecialChar(people);
-        changeId = removeNum(changeId)
-        resultArray.push(changeId);
-    }
-    return resultArray;
+    const changeId = inputArray.map((people) => {
+        let reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim;
+        let num = /[0-9]/g;
+        people = people.replace(reg, "").replace(num, "");
+        return people
+    })
+    return changeId;
 }
-
-// 특수문자 제거
-function removeSpecialChar(text) {
-    let reg = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim;
-    let changeText = text.replace(reg, "");
-    return changeText;
-}
-
-// 숫자제거
-function removeNum(text) {
-    let resultText = '';
-    for(const piece of text){
-        if(isNaN(piece)){
-            resultText += piece
-        }
-    }
-    return resultText
-}
-
-console.log(filterId(peoples))
