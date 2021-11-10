2e530c8d8582e31303ceabc0b4cdae0d848e26ff
ju-kkim <wndus8198@naver.com>
2021-11-10 23:57:31 +0900

    코드 전체 수정

--- 04_checkBracket/checkBracket.js
+++ 04_checkBracket/checkBracket.js
@@ -1,26 +1,51 @@
+class Stack {
+    constructor() {
+        this.arr = [];
+    }
+    push(item) {
+        this.arr.push(item)
+    }
+    pop() {
+        return this.arr.pop();
+    }
+    peek() {
+        return this.arr[this.arr.length - 1];
+    }
+}
+
 const data = "[1,2,[3,4,[5,[6]]]]"
-function parsing(string) {
-    let startArray = [];
-    let finishArray = [];
-    let parsingResult = [];
-    for(let ele of string){
-        switch (ele) {
+function run(arr) {
+    let bracketStack = new Stack;
+    let depthCount = 0;
+    let eleCount = 0;
+    for(let item of arr) {
+        switch (item) {
             case '[' :
-                startArray.push(ele);
+                depthCount += 1;
+                bracketStack.push(item)
                 break
             case ']' :
-                finishArray.push(ele);
+                if(bracketStack.arr.length > 0){
+                    bracketStack.pop()
+                }else {
+                    return '여는 괄호가 부족합니다.'
+                }
                 break
             case ',' :
                 break
             default :
-                parsingResult.push(ele)
+                eleCount += 1;
         }
     }
-    if(startArray.length === finishArray.length){
-        return `배열의 중첩된 깊이 수준은 ${startArray.length}이며, 총 ${parsingResult.length}개의 원소가 포함되어 있습니다`
-    }else {
-        return '오류가 있습니다'
+    const errorMsg = checkError(bracketStack);
+    const resultMsg = `배열의 중첩된 깊이 수준은 ${depthCount}이며, 총 ${eleCount}개의 원소가 포함되어 있습니다`;
+
+    return errorMsg === undefined ?  resultMsg : errorMsg
+}
+
+function checkError(stack) {
+    if(stack.arr.length > 0) {
+        return '닫는 괄호가 부족합니다'
     }
 }
-console.log(parsing(data))
+console.log(run(data))
