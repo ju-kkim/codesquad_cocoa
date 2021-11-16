9a8f55b1b5e73094207a7769be7f85b4c72a3c3d
ju-kkim <wndus8198@naver.com>
2021-11-16 23:50:50 +0900

    data 이용해서 확률 %로 계산하기 추가

--- 05_asyn/asyn02.js
+++ 05_asyn/asyn02.js
@@ -24,11 +24,22 @@ class GradeProgram {
         const standardDeviation = this.getStandardDeviation();
         const minScore = ((min - average) / standardDeviation).toFixed(2);
         const maxScore = ((max - average) / standardDeviation).toFixed(2);
-
-        return {
-            minScore : minScore ,
-            maxScore : maxScore
+        const minPercent = this.changePercentage(minScore);
+        const maxPercent = this.changePercentage(maxScore);
+        return `${(maxPercent - minPercent) * 100}%`
+    }
+    changePercentage(num) {
+        const data = require('./data');
+        const decimalLastPoint = String(num).split('').pop();
+        let targetData = String(num).substr(0,3);
+        
+        if(num < 0){
+            num = Math.abs(num);
+            targetData = String(num).substr(0,3);
+            return 1 - data[targetData][decimalLastPoint];
         }
+        
+        return data[targetData][decimalLastPoint];
     }
 }
 
@@ -40,18 +51,18 @@ function getUserInput(){
     });
     let userInput = []
     rl.on('line', function (line) {
-            userInput.push(line)
+            userInput.push(line);
         })
         .on('close', function () {
             const userSubject = userInput.shift();
-            const userScore = new GradeProgram(userInput, userSubject)
-            console.log('userScore' , userScore)
+            const userScore = new GradeProgram(userInput, userSubject);
+            console.log('userScore' , userScore);
             console.log('과목 : ' , userScore.subject);
-            console.log('평균 : ' , userScore.getMean()) //77.87
-            console.log('표준편차 : ' , userScore.getStandardDeviation()) //7.67
-            console.log('70 ~ 80점 비율' , userScore.getPercentage(70, 80))
+            console.log('평균 : ' , userScore.getMean());
+            console.log('표준편차 : ' , userScore.getStandardDeviation());
+            console.log('70 ~ 80점 비율' , userScore.getPercentage(70, 80));
             process.exit();
         });
 }
 
-getUserInput()
+getUserInput();
