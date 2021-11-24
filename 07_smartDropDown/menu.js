acb7a23bea8b3072cf654858fbe42067bb9e7d53
ju-kkim <wndus8198@naver.com>
2021-11-24 22:42:09 +0900

    fix : mouse evnet mouseenter, mouseleave 수정

--- 07_smartDropDown/menu.js
+++ 07_smartDropDown/menu.js
@@ -1,11 +1,11 @@
 function hoverHandler() {
+    const menu = this.parentElement;
     let showDepth02 = setTimeout(() => {
-        this.classList.add('active')
+        menu.classList.add('active')
     }, 1000);
-
-    this.addEventListener('mouseout', function(){
-        if(this.classList.contains('active')) {
-            setTimeout(() => { this.classList.remove('active') }, 500)
+    menu.addEventListener('mouseleave', function(){
+        if(menu.classList.contains('active')) {
+            setTimeout(() => { menu.classList.remove('active') }, 500)
             return
         }
         clearTimeout(showDepth02)
@@ -13,7 +13,7 @@ function hoverHandler() {
 }
 
 function dropDownMenu() {
-    const depth01 = document.querySelector('.depth01_wrap');
-    depth01.addEventListener('mouseover', hoverHandler)
+    const depth01 = document.querySelector('.depth01_wrap .depth01');
+    depth01.addEventListener('mouseenter', hoverHandler)
 }
 dropDownMenu();
